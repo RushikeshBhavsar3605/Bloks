edb51972353d32d7117ed3f96fda4528455ebd1e
Rushikeh <rvbhavsar36@gmail.com>
2024-07-14 13:37:28 +0530

    Add: OAuth (Google & Github) providers

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,18 +1,36 @@
 "use client";
 
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex flex-col items-center w-full gap-y-2">
-      <Button size="sm" className="w-full" variant="outline" onClick={() => {}}>
+      <Button
+        size="sm"
+        className="w-full"
+        variant="outline"
+        onClick={() => onClick("google")}
+      >
         <FcGoogle className="h-4 w-4 mr-2" />
         <p className="text-xs">Continue with Google</p>
       </Button>
 
-      <Button size="sm" className="w-full" variant="outline" onClick={() => {}}>
+      <Button
+        size="sm"
+        className="w-full"
+        variant="outline"
+        onClick={() => onClick("github")}
+      >
         <FaGithub className="h-4 w-4 mr-2" />
         <p className="text-xs">Continue with Github</p>
       </Button>
