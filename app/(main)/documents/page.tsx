060995030a3b8d14eb8cb1cca154a5ec9f0e5a6a
Rushikeh <rvbhavsar36@gmail.com>
2024-08-09 15:16:09 +0530

    Add: Sidebar component in documents page

--- app/(main)/documents/page.tsx
+++ app/(main)/documents/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useCurrentUser } from "@/hooks/use-current-user";
+import { PlusCircle } from "lucide-react";
 import { signOut } from "next-auth/react";
+import Image from "next/image";
 
 const DocumentsPage = () => {
   const user = useCurrentUser();
@@ -10,12 +13,37 @@ const DocumentsPage = () => {
     signOut();
   };
 
+  const onCreate = () => {
+    console.log("Page created!");
+  };
+
   return (
-    <div>
-      {JSON.stringify(user)}
-      <button onClick={onClick} type="submit">
-        Sign out
-      </button>
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.name?.split(" ")[0]}
+        &apos;s Jotion
+      </h2>
+
+      <Button onClick={onCreate}>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
     </div>
   );
 };
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Navigation } from "@/components/main/navigation";
+import { Spinner } from "@/components/spinner";
+import { useSession } from "next-auth/react";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
