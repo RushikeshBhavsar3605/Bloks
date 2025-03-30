83140eab8c3067aaf19d8c80795277ce19931015
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-03-30 21:34:32 +0530

    Fix: Modify the getDocument for handling parent document

--- actions/documents/get-documents.ts
+++ actions/documents/get-documents.ts
@@ -2,13 +2,23 @@
 
 import { currentUser } from "@/lib/auth";
 
-export const getDocuments = async () => {
+export const getDocuments = async (parentDocument?: string) => {
   const user = await currentUser();
+
   if (!user) {
     throw new Error("Unauthorized");
   }
 
-  const documents = await prisma?.document.findMany();
+  const documents = await prisma?.document.findMany({
+    where: {
+      userId: user.id,
+      parentDocumentId: parentDocument || undefined,
+      isArchived: false,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
   return documents;
 };
