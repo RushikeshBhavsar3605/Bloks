d1bef108672e8a394bc8442f5eb0269415c04f7e
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-04-14 19:29:48 +0530

    chore(email): replace Resend with Nodemailer for email delivery

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,35 +1,72 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
+import nodemailer from "nodemailer";
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_FROM,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const message = {
+    from: process.env.EMAIL_FROM,
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token}</p>`,
-  });
+    headers: {
+      "X-Entity-Ref-ID": "newmail",
+    },
+  };
+
+  await transporter.sendMail(message);
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_FROM,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const message = {
+    from: process.env.EMAIL_FROM,
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
-  });
+    headers: {
+      "X-Entity-Ref-ID": "newmail",
+    },
+  };
+
+  await transporter.sendMail(message);
 };
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_FROM,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const message = {
+    from: process.env.EMAIL_FROM,
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
-  });
+    headers: {
+      "X-Entity-Ref-ID": "newmail",
+    },
+  };
+
+  await transporter.sendMail(message);
 };
