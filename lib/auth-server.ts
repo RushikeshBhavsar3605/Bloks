382e6f9a9612ce2adbc7191b3dc1c716fed5f88c
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-10-22 20:44:14 +0530

    feat(lib): add cookieName in getToken

--- lib/auth-server.ts
+++ lib/auth-server.ts
@@ -17,6 +17,11 @@ export const currentUser = async (req: NextApiRequest) => {
     req: req as unknown as Request,
     // @ts-ignore
     secret: process.env.AUTH_SECRET,
+    // Use the correct cookie name for production
+    cookieName:
+      process.env.NODE_ENV === "production"
+        ? "__Secure-authjs.session-token" // Render production
+        : "authjs.session-token", // Local
   });
 
   console.log("Token in pages/api:", token);
